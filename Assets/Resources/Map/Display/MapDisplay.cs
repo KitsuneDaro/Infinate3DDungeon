121202659d3c1c6e77b537c1becb641d6ca56601
Soybeanman504 <bigmameman@gmail.com>
2025-01-23 23:32:59 +0900

    4th commit

--- Assets/Resources/Map/Display/MapDisplay.cs
+++ Assets/Resources/Map/Display/MapDisplay.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Player player;
-    public const int radius = 10;
-    public const int displaySize = 2 * radius + 1;
+    public Camera camera;
+
+    private const int radius = 5;
+    private const int displaySize = 2 * radius + 1;
     private Vector3 playerIntPosition;
     private Block[,,] holdingBlockArray = new Block[displaySize, displaySize, displaySize];
     
     // Start is called before the first frame update
     void Start()
     {
-        DrawFirst(new Vector3(0, 0.25f, 0.5f));
+        DrawFirst(player.transform.position);
     }
 
     // Update is called once per frame
@@ -33,27 +35,44 @@ public class MapDisplay : MonoBehaviour
 
         for (int xyz = 0; xyz < 3; xyz++) {
             int deltaPlayerIntPositionXYZ = (int)deltaPlayerIntPosition[xyz];
-            nowDeltaPlayerIntPosition[xyz] = deltaPlayerIntPositionXYZ;
-
+            
             if (deltaPlayerIntPositionXYZ == 0) {
                 continue;
             }
 
-            Vector3Int deleteingBoundsIntSize = Vector3Int.one * (displaySize - 1);
-            deleteingBoundsIntSize[xyz] = -deltaPlayerIntPositionXYZ;
+            Vector3Int deleteingBoundsIntSize = Vector3Int.one * displaySize;
+            deleteingBoundsIntSize[xyz] = deltaPlayerIntPositionXYZ;
 
             Vector3Int deleteingBoundsIntPosition = Vector3Int.one * -radius;
             if (deltaPlayerIntPositionXYZ < 0){
                 deleteingBoundsIntPosition[xyz] *= -1;
+                deleteingBoundsIntPosition[xyz] += 1;
             }
 
             BoundsInt deletingBounds = new BoundsInt(deleteingBoundsIntPosition, deleteingBoundsIntSize);
             Debug.Log(deletingBounds.min);
             Debug.Log(deletingBounds.max);
-            Debug.Log(deleteingBoundsIntSize);
+
+            DeleteBlocks(playerIntPosition + nowDeltaPlayerIntPosition, deletingBounds);
+
+            nowDeltaPlayerIntPosition[xyz] = deltaPlayerIntPositionXYZ;
+
+
+            Vector3Int addingBoundsIntSize = Vector3Int.one * displaySize;
+            addingBoundsIntSize[xyz] = -deltaPlayerIntPositionXYZ;
+
+            Vector3Int addingBoundsIntPosition = Vector3Int.one * -radius;
+            if (deltaPlayerIntPositionXYZ > 0){
+                addingBoundsIntPosition[xyz] *= -1;
+                addingBoundsIntPosition[xyz] += 1;
+            }
+
+            BoundsInt addingBounds = new BoundsInt(addingBoundsIntPosition, addingBoundsIntSize);
+            Debug.Log(addingBounds.min);
+            Debug.Log(addingBounds.max);
             
-            DeleteBlocks(playerIntPosition + nowDeltaPlayerIntPosition + playerFractionalPosition, deletingBounds);
-            AddBlocks(playerIntPosition + nowDeltaPlayerIntPosition + playerFractionalPosition, deletingBounds);
+            AddBlocks(playerIntPosition + nowDeltaPlayerIntPosition, addingBounds);
+
         }
         
         playerIntPosition = newPlayerIntPosition;
@@ -61,7 +80,7 @@ public class MapDisplay : MonoBehaviour
     
     void DrawFirst(Vector3 playerPosition)
     {
-        BoundsInt bounds = new BoundsInt(Vector3Int.one * -radius, Vector3Int.one * (displaySize - 1));
+        BoundsInt bounds = new BoundsInt(Vector3Int.one * -radius, Vector3Int.one * displaySize);
 
         AddBlocks(playerPosition, bounds);
     }
@@ -75,39 +94,64 @@ public class MapDisplay : MonoBehaviour
             for (int y = -radius; y <= radius; y++) {
                 for (int z = -radius; z <= radius; z++) {
                     Vector3 relativeBlockIntPosition = new Vector3(x, y, z);
-                    Vector3 relativeBlockPosition = relativeBlockIntPosition + playerFractionalPosition;
+                    Vector3 relativeBlockPosition = relativeBlockIntPosition - playerFractionalPosition;
                     Vector3 blockPosition = relativeBlockIntPosition + newPlayerIntPosition;
                     Vector3 repeatedBlockPosition = RepeatBlockPosition(blockPosition);
                     Block block = GetHoldingBlock(repeatedBlockPosition);
                     
-                    float alpha = 1.0f;
+                    float alpha = Mathf.Min(
+                        getAlphaBySphere(relativeBlockPosition, radius),
+                        Mathf.Max(
+                            getAlphaByPlane(relativeBlockPosition, new Vector3(0.0f, 1.0f, 0.0f), 0.0f),
+                            getAlphaByPlane(relativeBlockPosition, camera.transform.position - playerPosition, 2.0f)
+                        )
+                    );
 
-                    if (relativeBlockPosition.magnitude > radius + 0.5f) {
-                        alpha = 0.0f;
-                    } else {
-                        if (relativeBlockPosition.magnitude > radius - 0.5f) {
-                            alpha = radius + 0.5f - relativeBlockPosition.magnitude;
-                        }
-                    }
-
-                    if (alpha < 1.0f) {
-                        block.SetColor(new Color(1.0f, 0.0f, 1.0f, alpha));
+                    if (block != null) {
+                        block.SetColor(new Color(1.0f, 1.0f, 1.0f, alpha));
                     }
                 }
             }
         }
     }
 
+    float getAlphaBySphere(Vector3 relativeBlockPosition, int radius) {
+        float alpha = 1.0f;
+
+        if (relativeBlockPosition.magnitude > radius + 0.5f) {
+            alpha = 0.0f;
+        } else {
+            if (relativeBlockPosition.magnitude > radius - 0.5f) {
+                alpha = -relativeBlockPosition.magnitude + radius + 0.5f;
+            }
+        }
+        
+        return alpha;
+    }
+
+    float getAlphaByPlane(Vector3 relativeBlockPosition, Vector3 normal, float margin) {
+        float distance = Vector3.Dot(relativeBlockPosition.normalized, normal);
+        float alpha = 0.0f;
+
+        if (distance < -margin) {
+            alpha = 1.0f;
+        } else if (distance >= margin) {
+            alpha = 0.0f;
+        } else {
+            alpha = (margin - distance) / (2 * margin);
+        }
+
+        return alpha;
+    }
+
     void AddBlocks(Vector3 playerPosition, BoundsInt drawingBounds)
     {
         Vector3 newPlayerIntPosition = Vector3Utils.Floor(playerPosition);
-        Vector3 playerFractionalPosition = Vector3Utils.GetFractionalPart(playerPosition);
-
-        for (int x = drawingBounds.min.x; x <= drawingBounds.max.x; x++) {
-            for (int y = drawingBounds.min.y; y <= drawingBounds.max.y; y++) {
-                for (int z = drawingBounds.min.z; z <= drawingBounds.max.z; z++) {
+        
+        for (int x = drawingBounds.min.x; x < drawingBounds.max.x; x++) {
+            for (int y = drawingBounds.min.y; y < drawingBounds.max.y; y++) {
+                for (int z = drawingBounds.min.z; z < drawingBounds.max.z; z++) {
                     Vector3 relativeBlockIntPosition = new Vector3(x, y, z);
-                    Vector3 relativeBlockPosition = relativeBlockIntPosition + playerFractionalPosition;
                     Vector3 blockPosition = relativeBlockIntPosition + newPlayerIntPosition;
                     Vector3 repeatedBlockPosition = RepeatBlockPosition(blockPosition);
                     
@@ -124,16 +168,15 @@ public class MapDisplay : MonoBehaviour
 
     void DeleteBlocks(Vector3 playerPosition, BoundsInt drawingBounds) {
         Vector3 newPlayerIntPosition = Vector3Utils.Floor(playerPosition);
-        Vector3 playerFractionalPosition = Vector3Utils.GetFractionalPart(playerPosition);
-
-        for (int x = drawingBounds.min.x; x <= drawingBounds.max.x; x++) {
-            for (int y = drawingBounds.min.y; y <= drawingBounds.max.y; y++) {
-                for (int z = drawingBounds.min.z; z <= drawingBounds.max.z; z++) {
+        
+        for (int x = drawingBounds.min.x; x < drawingBounds.max.x; x++) {
+            for (int y = drawingBounds.min.y; y < drawingBounds.max.y; y++) {
+                for (int z = drawingBounds.min.z; z < drawingBounds.max.z; z++) {
                     Vector3 relativeBlockIntPosition = new Vector3(x, y, z);
                     Vector3 blockPosition = relativeBlockIntPosition + newPlayerIntPosition;
                     Vector3 repeatedBlockPosition = RepeatBlockPosition(blockPosition);
                     
-                    SetHoldingBlock(repeatedBlockPosition, null);
+                    DeleteHoldingBlock(repeatedBlockPosition);
                 }
             }
         }
@@ -148,10 +191,14 @@ public class MapDisplay : MonoBehaviour
     }
 
     void SetHoldingBlock(Vector3 repeatedBlockPosition, Block block) {
-        if (block == null) {
-            holdingBlockArray[(int)Mathf.Floor(repeatedBlockPosition.x), (int)Mathf.Floor(repeatedBlockPosition.y), (int)Mathf.Floor(repeatedBlockPosition.z)].DeleteMesh();
-        }
-
         holdingBlockArray[(int)Mathf.Floor(repeatedBlockPosition.x), (int)Mathf.Floor(repeatedBlockPosition.y), (int)Mathf.Floor(repeatedBlockPosition.z)] = block;
+    }
+
+    void DeleteHoldingBlock(Vector3 repeatedBlockPosition) {
+        Block holdingBlock = holdingBlockArray[(int)Mathf.Floor(repeatedBlockPosition.x), (int)Mathf.Floor(repeatedBlockPosition.y), (int)Mathf.Floor(repeatedBlockPosition.z)];
+        if (holdingBlock != null) {
+            holdingBlock.DeleteMesh();
+            SetHoldingBlock(repeatedBlockPosition, null);
+        }
     }
 }
